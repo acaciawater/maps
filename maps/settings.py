b85be62e9d51602f3527002bdeee181e7e1f6738
Theo Kleinendorst <theo.kleinendorst@acaciawater.com>
2019-10-30 17:25:03 +0100

    adds database to settings.py

--- maps/settings.py
+++ maps/settings.py
@@ -118,4 +118,18 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'postgres': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'maps',
+    }
+}
+
 from .secrets import *
