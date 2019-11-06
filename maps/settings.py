e2400e209e9a6097526ff26e85837331499f5c0e
Theo Kleinendorst <theo.kleinendorst@acaciawater.com>
2019-11-06 22:13:33 +0100

    databases moved from secrets to settings

--- maps/settings.py
+++ maps/settings.py
@@ -118,5 +118,18 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+DATABASES = {
+    'sqlite': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'maps',
+    }
+}
 
 from .secrets import *
